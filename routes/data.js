f64d73a5f5fde6361c988fe3f7f25bb1dffeb93c
artturi_S <kmasuo@utu.fi>
2019-11-17 14:51:04 +0200

    data return modified again3

--- routes/data.js
+++ routes/data.js
@@ -20,22 +20,22 @@ router.get('/', function(req, res, next) {
             console.log("db connection error...")
             console.log(err);
             res.status(500).send(err);
+        } else {
+            con.query("SELECT * FROM " + process.env.dbTableName + ";", function (err, result, fields) {
+                if (err){
+                    console.log("db select query error: " + err);
+                    res.status(500).send(err);
+                } else {
+                    var returnData = {data: []}
+                    console.log(result);
+                    result.forEach(entry => {
+                        console.log("Handling entry : " + entry);
+                        returnData.data.push(entry)
+                    })
+                    res.status(200).send(returnData);
+                }
+            });
         }
-        con.query("SELECT * FROM " + process.env.dbTableName + ";", function (err, result, fields) {
-            if (err){
-                console.log("db select query error: " + err);
-                res.status(500).send(err);
-            } else {
-                var returnData = {data: []}
-                console.log(result);
-                result.forEach(entry => {
-                    console.log("Handling entry : " + entry);
-                    returnData.data.push(entry)
-                })
-                res.status(200).send(returnData);
-            }
-
-        });
     });
     con.close();
 });
@@ -46,13 +46,14 @@ router.post('/',jsonParser, function(req, res){
             console.log("db connection error...")
             console.log(err);
             res.status(500).send(err);
-        }
-        if (req.body.add) {
-            con.query("INSERT INTO " + process.env.dbTableName + " VALUES (" + req.body.name + "," + req.body.score + ");", function (err, result, fields) {
-                if (err) throw err;
-                console.log(result);
-                res.data(result).send();
-            });
+        } else {
+            if (req.body.add) {
+                con.query("INSERT INTO " + process.env.dbTableName + " VALUES (" + req.body.name + "," + req.body.score + ");", function (err, result, fields) {
+                    if (err) throw err;
+                    console.log(result);
+                    res.data(result).send();
+                });
+            }
         }
     });
     con.close();
