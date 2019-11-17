5cbb29eab02a698f52f782ccd4918ef8b3124fbc
artturi_S <kmasuo@utu.fi>
2019-11-17 14:29:12 +0200

    helperAddr

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Scoreboard', port: process.env.PORT });
+  res.render('index', { title: 'Scoreboard', port: process.env.PORT, helperAddr: process.env.helperAddr });
 });
 
 module.exports = router;
