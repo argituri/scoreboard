962fe823bebc3776365bb7f4194da32f4c6f1068
artturi_S <kmasuo@utu.fi>
2019-11-17 15:34:43 +0200

    data adding modified7

--- public/javascripts/helpers.js
+++ public/javascripts/helpers.js
@@ -2,8 +2,12 @@
 function send(addr, port){
     var name = document.getElementById("nameInput").value;
     var score = document.getElementById("scoreInput").value;
+    var url = 'http://' + addr +  ': ' + port + '/data'
     console.log("Adding to db: " + name + ", " + score);
-    $.post('http://' + addr +  ': ' + port + '/data', {name, score})
+    console.log("To url : " + url);
+    $.post(url, {name, score}, "json")
+        .always(function(response) { console.log(response); })
+    /*$.post(, {name, score})
         .done(function( data ) {
             console.log( "call made " + data );
         });
